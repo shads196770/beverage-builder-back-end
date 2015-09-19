82380995a8f65528fbdf99f2a60f45589be31738
Bernard Lee <bernardlee82@gmail.com>
2015-09-18 21:27:11 -0400

    Create routes for each model
    Create all routes for each model. Restrictions will come later on.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  routes = [:beverages, :ingredients, :recipes]
+
+  routes.each do |route|
+    resources route
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
