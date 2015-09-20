a1182dfe364fce9a73632f05cea80c48e21dc023
Bernard Lee <bernardlee82@gmail.com>
2015-09-20 01:33:13 -0400

    Add seeds for the three existing models
    Add seeds to create some ingredients, beverages, and recipes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,81 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Ingredient seeds.
+ingredients = [
+  'Chocolate Liqueur',
+  'Coconut Milk',
+  'Cream',
+  'Dry Vermouth',
+  'Gin',
+  'Lime Juice',
+  'Orange Juice',
+  'Pineapple Juice',
+  'Simple Syrup',
+  'Vodka',
+  'White Rum'
+]
+
+# Beverage seeds.
+beverages = [
+  {
+    name: 'Alexander',
+    ingredients: [
+      { name: 'chocolate liqueur', parts: 1 },
+      { name: 'gin', parts: 1 },
+      { name: 'cream', parts: 1 }
+    ]
+  },
+  {
+    name: 'Daiquiri',
+    ingredients: [
+      { name: 'white rum', parts: 9 },
+      { name: 'lime juice', parts: 5 },
+      { name: 'simple syrup', parts: 3 }
+    ]
+  },
+  {
+    name: 'Martini',
+    ingredients: [
+      { name: 'gin', parts: 6 },
+      { name: 'dry vermouth', parts: 1 }
+    ]
+  },
+  {
+    name: 'Piña Colada',
+    ingredients: [
+      { name: 'white rum', parts: 1 },
+      { name: 'coconut milk', parts: 1 },
+      { name: 'pineapple juice', parts: 3 }
+    ]
+  },
+  {
+    name: 'Screwdriver',
+    ingredients: [
+      { name: 'vodka', parts: 2 },
+      { name: 'orange juice', parts: 1 }
+    ]
+  }
+]
+
+# Models
+models = [Beverage, Ingredient]
+
+# Destroy all database records.
+# Recipe records are destroyed when their associated beverage record is destroyed.
+models.each(&:destroy_all)
+
+# Seed ingredients.
+ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
+
+# Seed beverages and recipes.
+beverages.each do |beverage|
+  next if Beverage.exists?(['lower(name) = ?', beverage[:name].downcase])
+
+  current_beverage = Beverage.create(name: beverage[:name])
+
+  beverage[:ingredients].each do |ingredient|
+    recipe = Recipe.new
+    recipe.beverage = current_beverage
+    recipe.ingredient = Ingredient.find_by('lower(name) = ?', ingredient[:name].downcase)
+    recipe.parts = ingredient[:parts]
+    recipe.save
+  end
+end
