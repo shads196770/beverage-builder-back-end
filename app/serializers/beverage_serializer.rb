7696baa89cdf0b96bae08b71aabdae98e6429293
Bernard Lee <bernardlee82@gmail.com>
2015-09-22 13:04:33 -0400

    Modify beverage serializer
    Expose ingredient id.

--- app/serializers/beverage_serializer.rb
+++ app/serializers/beverage_serializer.rb
@@ -4,6 +4,6 @@ class BeverageSerializer < ActiveModel::Serializer
   has_many :recipes, root: :recipe
 
   def recipes
-    object.recipes.map { |i| { name: i.ingredient.name, parts: i.parts } }
+    object.recipes.map { |i| { id: i.ingredient.id, name: i.ingredient.name, parts: i.parts } }
   end
 end
