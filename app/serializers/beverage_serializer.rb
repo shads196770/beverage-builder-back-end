5284b75f9982b02ca0850dfeebfc793892615199
Bernard Lee <bernardlee82@gmail.com>
2015-09-22 02:22:59 -0400

    Add serialization to beverages
    Beverages will be rendered as objects with a root key of "beverage"
    or "beverages" that is an array of objects representing the beverage and
    its recipe. The beverage objects have a key, "name", with its name as
    the value and a key, "recipe", that is an array of objects representing
    the ingredients. The ingredients object has a key, "name", with its name
    as the value, and a key, "parts", with the amount of relative parts to
    be used.

--- app/serializers/beverage_serializer.rb
+++ app/serializers/beverage_serializer.rb
@@ -1,2 +1,9 @@
 class BeverageSerializer < ActiveModel::Serializer
+  attributes :name
+
+  has_many :recipes, root: :recipe
+
+  def recipes
+    object.recipes.map { |i| { name: i.ingredient.name, parts: i.parts } }
+  end
 end
