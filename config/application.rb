60a7f38ebaa789f25091669ae1608c30a386699f
Bernard Lee <bernardlee82@gmail.com>
2015-09-27 14:30:13 -0400

    Modify CORS methods
    Allow POST, GET, PATCH, and DELETE methods.

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module BeverageBuilderApi
     config.middleware.insert_before 0, 'Rack::Cors' do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :delete, :patch, :post]
       end
     end
   end
