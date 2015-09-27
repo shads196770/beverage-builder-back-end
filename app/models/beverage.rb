5982225e0afc689b45b99fb1b97689cd1a79fa72
Bernard Lee <bernardlee82@gmail.com>
2015-09-27 15:59:03 -0400

    Add validation to three models
    Add presence and uniqueness validations to the following models:
    Account: email
    Beverage: name
    Ingredient: name

--- app/models/beverage.rb
+++ app/models/beverage.rb
@@ -1,4 +1,6 @@
 class Beverage < ActiveRecord::Base
   has_many :recipes, inverse_of: :beverage, dependent: :destroy
   has_many :ingredients, through: :recipes
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ActiveRecord::Base
   has_many :recipes, inverse_of: :ingredient
   has_many :beverages, through: :recipes
+
+  validates :name, presence: true, uniqueness: true
 end
